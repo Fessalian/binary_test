61f792b84b19fbfa04bbf164f365d2fe1f5fb7d7
Vasilchenko Vitaliy <vet.vasilchenko@gmail.com>
2014-01-22 00:06:55 +0200

    add angular routes

--- app/assets/javascripts/config.js
+++ app/assets/javascripts/config.js
@@ -2,6 +2,14 @@
 
 testApp.config(
   function( $routeProvider, $locationProvider ) {
+    $routeProvider
+      .when('/',                  { template: JST['views/recipes_list'] })
+      .when('/recipes/:id',       { template: JST['views/recipes_show'] })
+      .when('/recipes/new/:id',   { template: JST['views/recipes_new'] })
+      .when('/recipes/edit/:id',  { template: JST['views/recipes_edit'] })
+      .otherwise({redirectTo: '/'});
+
     $locationProvider.html5Mode(true);
   }
 );
+
