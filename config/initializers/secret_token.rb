58a6464d36bf809e2bd395ee8af47650ab760c27
Vasilchenko Vitaliy <vet.vasilchenko@gmail.com>
2014-01-21 23:05:18 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BinaryTestApp::Application.config.secret_key_base = '013e461566f1d4b1878791b034de149d159724fb935d10c3ccc530e0d382c421aa41ca4750bf45b23aa50a0dc94f92b3cdfc3e30c07d481001d1ca0eccbd6847'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BinaryTestApp::Application.config.session_store :cookie_store, key: '_session'
